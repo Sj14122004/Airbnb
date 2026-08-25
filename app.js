e758ab0c6591303817f0d312678229c8a467f5a6
Sj14122004 <joshishivam2004@gmail.com>
2026-08-26 04:35:14 +0530

    validation

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./Schema.js");
-
+const {listingSchema, reviewSchema} = require("./Schema.js");
+const Review = require("./models/review.js");
 
 main()
 .then((res) => console.log("connected to db"))
@@ -36,12 +36,20 @@ const validateListing = (req,res,next) => {
     let {error} = listingSchema.validate(req.body);
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
-        return next(new ExpressError(400, error));
+        return next(new ExpressError(400, errMsg));
     }
         next();
     
 }
 
+const validateReview = (req,res,next) => {
+    let {error} = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        return next(new ExpressError(400,errMsg));
+    }
+    next();
+}
 //index route
 app.get("/listings", wrapAsync(async(req,res) => {
     const allListings = await Listing.find({});
@@ -75,10 +83,37 @@ app.delete("/listings/:id", wrapAsync(async(req,res) => {
     res.redirect("/listings")
 })
 );
+
+    //revieww route
+app.post("/listings/:id/reviews",
+    validateReview,
+    wrapAsync (async (req,res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`)
+}));
+
+//review delete
+app.delete("/listings/:id/reviews/:reviewId",
+    wrapAsync(async(req,res) => {
+        let {id, reviewId} = req.params;
+
+        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+        await Review.findByIdAndDelete(reviewId);
+
+        res.redirect(`/listings/${id}`);
+    })
+)
 //show route
 app.get("/listings/:id", wrapAsync(async(req,res) => {
     let {id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {listing})
 })
 );
@@ -121,6 +156,6 @@ app.use((err,req,res,next) => {
 });
 
 
-app.listen(8000, () => {
+app.listen(3000, () => {
     console.log("server is running")
 });
